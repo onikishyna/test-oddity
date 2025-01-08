887caf75ddbacf82cf90ced0dbcd65a72d91db85
Olena Nikishyna <lyashenko.ol.yur@gmail.com>
2025-01-08 18:07:29 +0200

    build

---
+++ dist/index.4b6250e3.js
@@ -0,0 +1,2 @@
+let e=!0;document.getElementById("change-logo-button").addEventListener("click",function(t){t.preventDefault();let n=document.getElementById("logo");e?n.src="/ODD_BIG.a24e177b.svg":n.src="/Google-Logo.wine.d5a2c166.svg",e=!e});
+//# sourceMappingURL=index.4b6250e3.js.map
